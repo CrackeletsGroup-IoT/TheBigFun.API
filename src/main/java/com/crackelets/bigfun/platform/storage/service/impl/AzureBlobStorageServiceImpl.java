39a7efd20a9aeaf515f4ed946247cecf14e7dcb5
ValeryAyasta <102206366+ValeryAyasta@users.noreply.github.com>
2024-05-25 20:35:22 -0500

    Update AzureBlobStorageServiceImpl.java

--- src/main/java/com/crackelets/bigfun/platform/storage/service/impl/AzureBlobStorageServiceImpl.java
+++ src/main/java/com/crackelets/bigfun/platform/storage/service/impl/AzureBlobStorageServiceImpl.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 
 @Service
 public class AzureBlobStorageServiceImpl implements AzureBlobStorageService {
-    private final String connectionString =
+    private final String string =
             "DefaultEndpointsProtocol=https;AccountName=thebigfun;AccountKey=bn/4CjRo9kKnEQ5aSKZlkDde9jPI6bvXX7if15AVqGD/3ezntog0RM+ppOd8w+IC90l5fYdO/ven+ASthVvDVA==;EndpointSuffix=core.windows.net";
     private final String containerName = "the-big-fun-files";
 
@@ -18,7 +18,7 @@ public class AzureBlobStorageServiceImpl implements AzureBlobStorageService {
 
     public String uploadFileToBlobStorage(String filename, InputStream inputStream, Integer length) {
         BlobClient blobClient = (new BlobClientBuilder()).
-                connectionString(connectionString).containerName(containerName).blobName(filename).buildClient();
+                connectionString(string).containerName(containerName).blobName(filename).buildClient();
         blobClient.upload(inputStream, (long)length, true);
         return blobClient.getBlobUrl();
     }
