616c5c2924b0aa59d984498a7c6e13ee454143e4
ValeryAyasta <102206366+ValeryAyasta@users.noreply.github.com>
2024-05-27 19:46:35 -0500

    feature create endpoint to generate qr code

--- src/main/java/com/crackelets/bigfun/platform/payment/api/rest/PaymentController.java
+++ src/main/java/com/crackelets/bigfun/platform/payment/api/rest/PaymentController.java
@@ -4,11 +4,16 @@ import com.crackelets.bigfun.platform.payment.domain.service.PaymentService;
 import com.crackelets.bigfun.platform.payment.mapping.PaymentMapper;
 import com.crackelets.bigfun.platform.payment.resource.CreatePaymentResource;
 import com.crackelets.bigfun.platform.payment.resource.PaymentResource;
+import com.crackelets.bigfun.platform.payment.resource.QRCodeRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/payments")
@@ -34,6 +39,40 @@ private final PaymentMapper mapper;
     public ResponseEntity<PaymentResource> createPayment(@RequestBody CreatePaymentResource resource){
         return  new ResponseEntity<>(mapper.toResource(paymentService.create(mapper.toModel(resource))), HttpStatus.CREATED);
     }
+
+    @Value("${qr.api.url}")
+    private String qrApiUrl;
+
+    //https://api.qr-code-generator.com/v1/create?access-token=your-acces-token-here
+
+    @Value("${qr.api.access.token}")
+    private String qrApiAccessToken;
+
+    @PostMapping("/generateQR")
+    public ResponseEntity<?> generateQRCode(@RequestParam("uuid") String uuid) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Construir la URL del servicio de generación de códigos QR
+        String url = qrApiUrl + "?access-token=" + qrApiAccessToken;
+
+        QRCodeRequest qrCodeRequest = new QRCodeRequest();
+        qrCodeRequest.setQr_code_text(uuid);
+        qrCodeRequest.setImage_format("PNG");
+
+        // Configurar los encabezados
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // Crear la entidad de la solicitud
+        HttpEntity<QRCodeRequest> requestEntity = new HttpEntity<>(qrCodeRequest, headers);
+
+        // Realizar la solicitud POST y obtener la respuesta como un arreglo de bytes
+        ResponseEntity<?> response = restTemplate.postForEntity(url, requestEntity, byte[].class);
+
+        // Devolver la respuesta recibida del servicio de generación de códigos QR
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
+    }
+
     @DeleteMapping("{paymentId}")
     public ResponseEntity<?> deletePayment(@PathVariable Long paymentId){
         return paymentService.delete(paymentId);
